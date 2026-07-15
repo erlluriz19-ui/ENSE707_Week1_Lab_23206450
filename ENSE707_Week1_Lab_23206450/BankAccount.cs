11fed71607867c3138270f92d2a3e3bbba0e80b2
erlluriz19-ui <erlluriz19@gmail.com>
2026-07-16 11:38:26 +1200

    committing answers and the given code  up to Activity 6)
    also added  comments to the BankAccount class

---
+++ ENSE707_Week1_Lab_23206450/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENSE707_Week1_Lab_23206450
+{
+    internal class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; private set; }
+
+        //initializes the fields of the bank account
+        public BankAccount(string accountHolder, decimal openingBalance)
+        {
+            AccountHolder = accountHolder;
+            Balance = openingBalance;
+        }
+
+        //calculates the sum current balance and the Deposit amount for the new balance
+        public void Deposit(decimal amount)
+        {
+            Balance = Balance + amount;
+        }
+
+        //Subtracts the withdrawal amount from balance for the new balance when user withdraw
+        public bool Withdraw(decimal amount)
+        {
+            Balance = Balance - amount;
+            return true;
+        }
+        //calculates the transaction fee by multiplying the amount by 2%
+        public decimal CalculateTransactionFee(decimal amount)
+        {
+            return amount * 0.02m;
+        }
+    }
+}
--- ENSE707_Week1_Lab_23206450/Program.cs
+++ ENSE707_Week1_Lab_23206450/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ENSE707 Bank Lab (Lab1)
+
+/* Activity 1
+ * 1) commiting regularly can make it easier to roll back to an older version if a fatal bug affecting the software 
+ * 
+ * 2) by showing that the person working on the project has been continuously working on it by making meaningful commits and showing
+ * what they have worked on in their session of writing up lines for the project
+ *
+ *Activity3
+ * 1) It would continue the calculations which would breach Functional Stability as the account would start in debt,
+ * this would likely be invalid for the system which could lead to incorrect account behaviour. fix by making 
+ * be validated first to ensure it is zero or more opening balance 
+ * 
+ * 2) it would continue on with the calculation which means it would treat it like it was a withdrawal which
+ *  would also breach Functional Stability. To fix this it should first validate if the deposit amount is positive
+ *  and is greater than 0
+ *  
+ *  3) The calculation would continue and would make the new balance go into the negatives
+ *  
+ *  4) Yes as it shows on the console the fee amount calculated from balance
+ *  
+ *  5) most likely as you would likely only need to make a few test cases in the main Program.cs
+ *   to test for bugs or for variables that shouldn't change in a certain  way
+ *   
+ *   6) functional requirements missing:
+ *   - withdrawing should always be positive 
+ *   - depositing should be positive
+ *   - account should be validated for its opening balance and current balance
+ *
+*/
+using ENSE707_Week1_Lab_23206450;
+
+BankAccount account = new BankAccount("Student User", 100);
+account.Deposit(-50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account holder: {account.AccountHolder}");
+Console.WriteLine($"Current balance: {account.Balance}");
+Console.WriteLine($"fee on $167: {account.CalculateTransactionFee(100)}");
